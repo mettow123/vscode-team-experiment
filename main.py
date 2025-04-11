6d7012671443c5bf5e796aa0b708647b50b368a0
mettow123 <16727210619@163.com>
2025-04-11 12:52:45 +0800

    Add initial main.py file

---
+++ main.py
@@ -0,0 +1,9 @@
+def greet(name):
+ print(f"Hello, {name}!")
+ greet("GitHub")
+ def greet(name):
+ print(f"Hello, {name}!")
+ def goodbye(name):
+ print(f"Goodbye, {name}!")
+ greet("GitHub")
+ goodbye("GitHub"）
